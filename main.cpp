78ed9bea76eae7ac4ba8165a527177e2f22d6067
mdepp <mdepp@ualberta.ca>
2017-11-02 21:25:36 -0600

    display silhouette of spheres

--- main.cpp
+++ main.cpp
@@ -1,10 +1,56 @@
 #include "sdlframework.h"
+#include "sphere.h"
+#include <glm/glm.hpp>
 
 int main(int argc, char* argv[])
 {
     WindowFramework* fw = new SDLFramework();
-    while(fw->tick());
-    delete fw;
 
+    const float viewportWidth = 10.f;
+    const float viewportHeight = viewportWidth/float(fw->width())*fw->height();
+    const float viewportDepth = 10.f;
+
+    fw->clear({0.f, 0.f, 0.f});
+    
+    const int nObjects = 3;
+    Object* objects[nObjects];
+    objects[0] = new Sphere({0.f, 0.f, 13.f}, 2.f);
+    objects[1] = new Sphere({ 0.f, 3.f, 13.f }, 1.f);
+    objects[2] = new Sphere({ 3.f, 2.f, 12.f }, 2.f);
+
+    bool running = true;
+
+    for (int pixelX=0; pixelX<fw->width(); ++pixelX)
+    {
+        for (int pixelY=0; pixelY<fw->height(); ++pixelY)
+        {
+            float x = (pixelX-fw->width()/2)*viewportWidth/fw->width();
+            float y = (pixelY-fw->height()/2)*viewportHeight/fw->height();
+            
+            Ray ray({0, 0, 0}, glm::normalize(glm::vec3(x, y, viewportDepth)));
+
+            glm::vec3 intersection;
+            for(auto object : objects)
+            {
+                if (object && object->intersect(ray, intersection))
+                {
+                    fw->drawPixel(pixelX, pixelY, {1.f, 1.f, 1.f});
+                }
+            }
+
+            running = fw->tick();
+            if (!running) break;
+        }
+        if (!running) break;
+    }
+
+    while (running)
+        running = fw->tick();
+
+    delete fw;
+    for(auto object : objects)
+        if (object)
+            delete object;
+    
     return 0;
 }
